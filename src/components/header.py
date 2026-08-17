ede5d5e97ad69660b323e5d2bc056cededd81426
Suryanxh <realthakursuryansh@gmail.com>
2026-08-17 10:51:40 +0530

    Overwriting Streamlit styles

---
+++ src/components/header.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def header_home():
+    st.markdown(
+        """
+        <div class="present-logo">
+            Present.ly
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
